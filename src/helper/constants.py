c7d8240d884c8626912f752d7699057f30855d08
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2025-03-25 17:49:08 +0530

    release workflow ++
    - compatibility changes

--- src/helper/constants.py
+++ src/helper/constants.py
@@ -1,5 +1,12 @@
 # pylint: disable =invalid-name
-from enum import StrEnum
+from enum import Enum
+
+try:
+    from enum import StrEnum
+except ImportError:
+
+    class StrEnum(str, Enum):
+        pass
 
 
 class Facet(StrEnum):
