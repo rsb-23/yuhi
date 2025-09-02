fe0daf2650341978f44705f383733a5a22f4eb8c
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2025-09-02 17:03:42 +0530

    Fixed test coverage run
    - Added repo cleaner
    - improved config

--- src/glossary.py
+++ src/glossary.py
@@ -3,6 +3,7 @@ import click
 DICTIONARY = {"facet": "A sub-component of a project"}
 
 
+# pylint: disable=r0903
 class Glossary(click.Group):
     """Dictionary of terms used in the help"""
 
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -19,4 +19,4 @@ def test_sample(runner, test_env):
 def test_scan(runner, test_env):
     response = runner.invoke(cli, ["scan"])
     assert response.exit_code == 0
-    assert "3 bad package(s)" in response.output
+    assert "2 bad package(s)" in response.output
