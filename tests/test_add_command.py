cca0546e327b28341ada0b7bd0a9e2b7d685d192
rsb-23 <57601627+rsb-23@users.noreply.github.com>
2025-09-02 21:26:58 +0530

    patched httpx

--- tests/test_add_command.py
+++ tests/test_add_command.py
@@ -1,5 +1,4 @@
 # pylint: disable=w0613
-
 import pytest
 
 from src.cli import cli
@@ -12,12 +11,21 @@ def assert_with_error(result, msg):
         assert "NotImplementedError" in result.output
 
 
-@pytest.mark.parametrize("facet", ["contribution", ".gitignore", "license", "readme", "sourcery"])
+@pytest.mark.parametrize("facet", ["contribution", ".gitignore", "readme", "sourcery"])
 def test_add_facet(test_env, runner, facet):
     """Test adding various facets."""
     result = runner.invoke(cli, ["add", facet])
     print(result.output)
     assert_with_error(result, "created")
+
+
+def test_add_license(test_env, runner, mock_httpx_get):
+    mock_httpx_get.return_value.json.return_value = {"body": "[year]"}
+    # testcase
+    result = runner.invoke(cli, ["add", "license"])
+    assert_with_error(result, "created")
+
+    mock_httpx_get.assert_called_once()
 
 
 def test_add_pre_commit(test_env, runner):
